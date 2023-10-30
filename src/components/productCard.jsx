947efd47b9af4e35686ffa427f86dc4ab57c6ee4
AurelienPREVOST <aurelienprevost77@gmail.com>
2023-10-30 17:40:12 +0100

    lazyLoadOK

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -1,12 +1,20 @@
 import { Link } from 'react-router-dom';
 import {config} from '../../config'
+import { LazyLoadImage } from "react-lazy-load-image-component";
+
 /* eslint-disable react/prop-types */
 
 const ProductCard = ({product}) => {
+
+
   return (
     <article key={product.id} className="eventCard">
       <Link to={`/detail/${product.id}`} aria-label={`Détails de l'évenement : ${product.name}`}>
-      <img src={config.pict_url + product.photo} alt={`Affiche d'illustration de l'évenement :  ${product.name}`} />
+      <LazyLoadImage src={config.pict_url + product.photo}
+        width={200} height={200}
+        alt={`Affiche d'illustration de l'évenement :  ${product.name}`}
+      />
+      {/* <img src={config.pict_url + product.photo} alt={`Affiche d'illustration de l'évenement :  ${product.name}`} /> */}
         <h2>{product.name}</h2>
         <p><b>{new Date(product.date).toISOString().split("T")[0]}</b></p>
         <p>{product.lieu}</p>
